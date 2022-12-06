098ed2ae5eb99d0e69b925610c2e1571c7163390
cksrb63 <cksr1@naver.com>
2022-12-06 11:06:27 +0900

    fix

--- src/page/Prolog.jsx
+++ src/page/Prolog.jsx
@@ -9,15 +9,14 @@ function Prolog() {
 
   console.log(2);
   const text = () => {
-    if (page === 0) return "프롤로그 페이지 1 - 당신은 후각을 잃었습니다.";
-    else if (page === 1) return "프롤로그 페이지 2 - 페이지 2";
-    else return "프롤로그 페이지 마지막";
+    if (page === 0) return "당신은 후각을 잃었습니다.";
+    else return "향기를 기억해내기 위해 추억을 되돌아보시겠습니까?";
   };
 
   const context = text();
 
   const prologClickHandler = () => {
-    if (page < 2) setPage(page + 1);
+    if (page < 1) setPage(page + 1);
     else navigate("/test");
   };
 
@@ -29,11 +28,6 @@ function Prolog() {
         </div>
       )}
       {page === 1 && (
-        <div className="home-pr" onClick={prologClickHandler}>
-          {context}
-        </div>
-      )}
-      {page === 2 && (
         <div className="home-pr" onClick={prologClickHandler}>
           {context}
         </div>
